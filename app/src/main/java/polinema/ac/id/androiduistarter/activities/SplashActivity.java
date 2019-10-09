c55caa5fba1387ebee9b0ef034c7c905cffee487
Abyan <harlem.minion09@gmail.com>
2019-10-09 15:19:30 +0700

    View Pager & Bottom Navigation

--- app/src/main/java/polinema/ac/id/androiduistarter/activities/SplashActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/activities/SplashActivity.java
@@ -14,10 +14,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this,WelcomeActivity.class));
                 finish();
             }
         },5000);
